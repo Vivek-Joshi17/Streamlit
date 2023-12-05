3e3726055055aa65c8ef9b480c9d47578e8647ad
Vivek Joshi <vivekjoshi210111242@gmail.com>
2023-12-05 12:18:40 +0530

    Widgets part2(single/multiple selection and slider)

--- widgets.py
+++ widgets.py
@@ -28,3 +28,19 @@ if st.checkbox("show/hide"):
 #working with  expander
 with st.expander("Python"):
     st.success("Hello Python")
+
+# working with select
+
+my_lang = ["python","julia","go","rust"]
+choice = st.selectbox("Language",my_lang)
+st.write("You have selected {}".format(choice))
+
+# working with multiple selection
+spoken_lang =("English","French","Hindi","Pahadi","Japanese")
+my_spoken_lang = st.multiselect("Spoken_lang",spoken_lang)
+
+#working with slider(int/float/data)
+age = st.slider("Age",1,100)
+
+#working with select slider (any datatype)
+color=st.select_slider("Choose color",options=["yellow","red","blue","black","white"],value=("black","yellow"))
