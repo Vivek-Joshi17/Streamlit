d1b28974f9c45b3a40a67ddefc755545f14b5391
Vivek-Joshi17 <Vivekjoshi210111242@gmail.com>
2024-02-03 17:55:18 +0530

    basic text func

---
+++ text.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+#st.text("One Piece")#for writing the text 
+#st.text("I'm gonna be the pirate{}".format(" King"))# for writing variable varible
+
+#st.header("One Piece") # for header
+
+st.title("One Piece")
+st.write(f"'One Piece' is a Japanese manga and anime series created by Eiichiro Oda. It follows the adventures of Monkey D. Luffy, a young and ambitious pirate with the goal of becoming the Pirate King by finding the legendary treasure known as the One Piece. Luffy sets sail with his diverse crew, the Straw Hat Pirates, each possessing unique abilities and dreams. The series is celebrated for its rich storytelling, intricate world-building, and compelling characters, addressing themes of friendship, freedom, and the pursuit of dreams. With a vast and immersive narrative, 'One Piece' has captivated audiences globally and holds the record as one of the longest-running and most successful manga and anime franchises.")
+st.markdown("---")
+
